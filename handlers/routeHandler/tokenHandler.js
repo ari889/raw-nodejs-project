540bbfa268959a4bbc721ef4ad107ebf7dc1df5c
Arijit Banarjee <ontikdev.2@gmail.com>
2022-07-07 12:53:49 +0600

    Check api

--- handlers/routeHandler/tokenHandler.js
+++ handlers/routeHandler/tokenHandler.js
@@ -182,7 +182,6 @@ handler._token.delete = (requestProperties, callback) => {
 handler._token.verify = (id, phone, callback) => {
     data.read('tokens', id, (err, tokenData) => {
         if (!err && tokenData) {
-            console.log(phone);
             if (parseJSON(tokenData).phone === phone && parseJSON(tokenData).expires > Date.now()) {
                 callback(true);
             } else {
--- helpers/environment.js
+++ helpers/environment.js
@@ -16,6 +16,7 @@ environments.staging = {
     port: 3000,
     envName: 'staging',
     secretKey: 'asdasdasfdsdfghfdfgdfg',
+    maxChecks: 5,
 };
 
 // production environment
@@ -23,6 +24,7 @@ environments.production = {
     port: 5000,
     envName: 'production',
     secretKey: 'gfdfgdfhfghjghjgrtyr',
+    maxChecks: 5,
 };
 
 // determine which environment was passed
